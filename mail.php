499e35b0f7bb1953aff3e59b1704094eef943bd6
Rolando Lopantzi <rlopantzi@gmail.com>
2018-10-28 08:20:54 -0700

    init commit
    Updated the mail.php file to redirect the email address

--- mail.php
+++ mail.php
@@ -1,15 +1,19 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$servings = $_POST['servings'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n Email: $email \n Phone: $phone \n Servings: $servings \n Message: $message";
-$mailTo = "banibake@banibakery.com";
-$subject = "Contact Form";
-$mailheader = "From: ".$email;
-mail($mailTo, $subject, $formcontent, $mailheader) or die("Error!");
-echo 'Thank you!';
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $servings = $_POST['servings'];
+    $message = $_POST['message'];
+
+    $formcontent=" From: $name \n Email: $email \n Phone: $phone \n Servings: $servings \n Message: $message";
+    $mailTo = "rolando@rolandolopantzi.com";
+    $subject = "Contact Form";
+    $mailheader = "From: ".$email;
+
+    mail($mailTo, $subject, $formcontent, $mailheader) or die("Error!");
+    echo 'Thank you!';
+}
 
 ?>
