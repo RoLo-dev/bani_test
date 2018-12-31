18468253841b5dbef42583047c31784b2febf158
Rolando Lopantzi <rlopantzi@gmail.com>
2018-12-30 21:24:58 -0800

    Added JS file

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// This is for the mobiel nav
+let mobileOnly = window.matchMedia('(maxwidth: 640px)');
+let open = document.getElementById('open');
+let close = document.getElementById('close');
+
+mobileOnly.addListener(screenSize);
+open.addEventListener('click', openNav);
+close.addEventListener('click', closeNav);
+
+function screenSize() {
+    if (mobileOnly.matches) {
+        openNav();
+        closeNav();
+        toggleFlavor();
+        toggleFilling();
+    } else {
+        open.style.display = 'none';
+    }
+};
+
+function openNav() {
+    document.getElementById('open').style.display = 'none';
+    document.getElementById('mobileNav').style.width = '100%';
+};
+function closeNav() {
+    document.getElementById('mobileNav').style.width = '0';
+    document.getElementById('open').style.display = 'block'
+}
+
+// This is for the Flavor & Filling buttons
+let flavorBtn = document.getElementById('flavors');
+let fillingBtn = document.getElementById('fillings');
+
+flavorBtn.addEventListener('click', toggleFlavor);
+fillingBtn.addEventListener('click', toggleFilling);
+
+function toggleFlavor() {
+    // if (flavorList.style.display === 'none') {
+    //     flavorList.style.display = 'block';
+    // } else {
+    //     flavorList.style.display = 'none';
+    // }
+    $('#listFlv').slideToggle('slow');
+};
+function toggleFilling() {
+    $('#listFill').slideToggle('slow');
+};
