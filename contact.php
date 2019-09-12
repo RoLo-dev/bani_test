30b78f9b0c8fc8053fd2795e340ac3cd6236839b
Rolando Lopantzi <rlopantzi@gmail.com>
2019-09-11 17:56:25 -0700

    Init commit

---
+++ contact.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Bani Bakery specializes in Custom cakes, Wedding cakes and delicious pastries using fine ingredients and everything made fresh and with love by order. Never compromising quality over quantity. Located in Santa Ana California and cater all of Orange County and Los Angeles County.">
+    <meta name="keywords" content="cakes,custom,wedding cakes,desserts,pastries,Orange County,OC,bakery,french,macarons,pastel,pan dulce,wedding,cookies,fresh,love,delivery, smash cake,asian,high end,Santa ana,California,Orange,Southern California,local bakery">
+
+    <title>Bani Bakery | Custom Cakes</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,600,800" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/png" href="images/bani_favicon.png">
+    <link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/queries.css">
+    <link rel="stylesheet" type="text/html" href="index.html">
+    <link rel="stylesheet" type="text/html" href="gallery.html">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="js/lightbox-plus-jquery.min.js"></script>
+
+</head>
+<body>
+    <section id="contact">
+        <div class="wrapper">
+            <h2>CONTACT</h2>
+            <p>Have any questions? Please feel free to send us any questions or comments. For a quote request or placing an order, please provide detailed information (cake flavor, filling, servings and picture) as well as the date of the event.
+            </br>
+            </br>
+            Place your cake and/or sweet table orders 7-10 days prior from due date. Pastry and cookie orders should be placed at least 5 days prior from due date. We do require a 50% deposit for any order over $75.</p>
+
+            <div class="cntctForm">
+                <form action="mail.php" method="POST">
+                    <input type="text" name="name" placeholder="Name">
+                    <input type="text" name="email" placeholder="Email">
+                    <input type="text" name="phone" placeholder="Phone">
+                    <input type="text" name="date" placeholder="Due Date">
+                    <textarea name="message" placeholder="Message"></textarea>
+                    <button type="submit" name="submit">SEND</button>
+                </form>
+            </div>
+
+            <div class="cntctInfo">
+                <div class="storehrs">
+                    <h4>Store Hours</h4>
+                    <li>Monday closed</li>
+                    <li>Tues - Fri: 9am - 7pm</li>
+                    <li>Sat - Sun: 8am - 3pm</li>
+                </div>
+                <div class="webPhone">
+                    <li>
+                        <i class="fas fa-globe-americas"></i>
+                        <a href="mailto:banibake@banibakery.com"> banibake@banibakery.com</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-phone-square"></i>
+                        <a href="tel:6572316211"> (657) 231 - 6211</a>
+                    </li>
+                </div>
+                <div class="address">
+                    <li>
+                        <i class="fas fa-map-marked-alt"></i>
+                        <a href="https://www.google.com/maps/place/3710+Westminster+Ave,+Santa+Ana,+CA+92703/@33.7592065,-117.9220215,17z/data=!3m1!4b1!4m5!3m4!1s0x80dcd80b68a29147:0x8f38e2865ae5971b!8m2!3d33.7592021!4d-117.9198328" target="_blank">3710 Westminster Ave. Ste. E,</br>Santa Ana, Ca, 92703</a>
+                    </li>
+                </div>
+                <div class="socialM">
+                    <h4>Follow us on Social Media</h4>
+                    <div class="icons">
+                        <li>
+                            <a href="https://www.facebook.com/banibakery/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                        </li>
+                        <li>                           
+                            <a href="https://www.instagram.com/banihouseofcakes/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li>                           
+                            <a href="https://www.yelp.com/biz/bani-bakery-santa-ana" target="_blank"><i class="fab fa-yelp"></i></a>
+                        </li>
+                    </div>
+                </div>
+                <div class="award">
+                    <img src="https://threebestrated.com/awards/cakes-santa_ana-2019-clr.svg" alt="Best Cakes in Santa Ana" class="pics">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+
+<footer>
+    <div class="wrapper ftContent">
+        <div class="ftLogo">
+            <a href="index.html"><img src="images/bani_logo_17.png" alt="Bani bakery logo" class="pics"></a>
+        </div>
+        <div class="ftLinks">
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="index.html#about" class="navLink">About</a></li>
+            <li><a href="index.html#lists" class="navLink">Flavors & Fillings</a></li>
+            <li><a href="contact.php" class="navLink">Contact</a></li>
+        </div>
+        <div class="copyRight">&copy; Copyright 2019 www.banibakery.com</div>
+    </div>
+</footer>
+
+</html>
