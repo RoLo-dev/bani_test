61237b002749e23b4df84c11bdde89613e98eca3
Rolando Lopantzi <rlopantzi@gmail.com>
2020-03-15 17:24:24 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 200, function(){
 
         window.location.hash = hash;
       });
