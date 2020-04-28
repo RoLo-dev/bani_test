b6f07349e9afdaa80a9e243d225d7b671a30073e
Rolando Lopantzi <rlopantzi@gmail.com>
2020-04-27 19:31:26 -0700

    init commit

--- js/script.js
+++ js/script.js
@@ -10,12 +10,16 @@ $('#filling-title').on('click', function(){
     $('#filling-list').slideToggle('slow');
 });
 // This is the code for the modal
-let swtBtnDesktop = document.getElementById('swtBtn-desktop');
-let modal = document.getElementById('modal');
-let modalContent = document.getElementById('modal-content');
+const desktopOnly = window.matchMedia('(min-width: 691px)');
+const swtBtnDesktop = document.getElementById('swtBtn-desktop');
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modal-content');
 
 window.addEventListener('click', outsideClick);
-swtBtnDesktop.addEventListener('click', openModal);
+
+if(desktopOnly.matches){
+    swtBtnDesktop.addEventListener('click', openModal);
+}
 
 function openModal() {
     modal.style.opacity = '1';
@@ -28,28 +32,19 @@ function outsideClick(e) {
     }
 }
 // This is for mobile
-let mobileOnly = window.matchMedia('(max-width: 690px)');
+const mobileOnly = window.matchMedia('(max-width: 690px)');
 const swtBtnMobile = document.getElementById('swtBtn-mobile');
 const openNavBtn = document.getElementById('mobile-openBtn');
 const mobileNav = document.getElementById('mobile-nav');
 const openIcon = document.querySelector('.openIcon');
 
-mobileOnly.addListener(function(){
-   if (mobileOnly.matches) {
-        swtBtnMobile.addEventListener('click', displaySweetList);
-    }
-});
+// mobileOnly.addListener(screenSize);
 // swtBtnMobile.addEventListener('click', displaySweetList);
 openNavBtn.addEventListener('click', openNavMenu);
 
-// function screenSize() {
-//     if (mobileOnly.matches) {
-//         swtBtnMobile.addEventListener('click', displaySweetList);
-//     }
-// };
-// if (mobileOnly.matches) {
-//       swtBtnMobile.addEventListener('click', displaySweetList);
-//   }
+if (mobileOnly.matches) {
+    swtBtnMobile.addEventListener('click', displaySweetList);
+}
 function openNavMenu(){
   openIcon.classList.toggle('animate');
   mobileNav.classList.toggle('show-nav')
@@ -60,5 +55,5 @@ function displaySweetList(){
 
 AOS.init({
     offset: 150,
-    duration: 350
+    duration: 450
 });
