6293b65f88da8df2f5e7935f0cbfc0c3d4e6e0ff
Rolando Lopantzi <rlopantzi@gmail.com>
2020-05-01 11:38:53 -0700

    init commit

--- test.php
+++ test.php
@@ -14,6 +14,7 @@
                 $msg = "Please enter a valid email address";
             } else {
                 $email_to = "banibake@banibakery.com";
+                // $email_to = "rlopantzi@gmail.com";
                 $email_subject = 'Contact form submission from '.$name;
                 $body =
                     '<h2>Contact Request</h2>
@@ -33,8 +34,8 @@
                     // echo '<a href="http://banibakery.com">Return home</a>';
                     // header("Location: http://banibakery.com/contact.php");
                     // exit();
-                    echo $msg = "Thank you, we will get back to you soon";
-                    echo $msgClass = "emailSuccess";
+                    $msg = "Thank you, we will get back to you soon";
+                    $msgClass = "emailSuccess";
                 } else {
                     // Failed
                     echo $msg = "Sorry, something went wrong. Please try again";
@@ -54,97 +55,29 @@
             echo $messageErr = "Message is required";
         }
     }
-// < ?p h p echo $_SERVER["PHP_SELF"]; ? >
-
-    // if (isset($_POST['submit'])) {
-    //     $name = $_POST['name'];
-    //     $email = $_POST['email'];
-    //     $phone = $_POST['phone'];
-    //     $date = $_POST['date'];
-    //     $message = $_POST['message'];
-    //     $nameErr = $emailErr = $phoneErr = $dateErr = $messageErr = "";
-
-    //     if(empty($name) && empty($email) && empty($phone) && empty($date) && empty($message)) {
-    //         // Failed
-    //         $nameErr = "Name is required";
-    //         $emailErr = "Email is required";
-    //         $phoneErr = "Phone number is required";
-    //         $dateErr = "Date is required";
-    //         $messageErr = "Message is required";
-    //     } else{
-    //         if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-    //             $emailErr = "Invalid email format";
-    //         } else {
-    //             $email_to = "banibake@banibakery.com";
-    //             $email_subject = 'Contact form submission from '.$name;
-    //             $body =
-    //                 '<h2>Contact Request</h2>
-    //                 <h4>Name</h4><p>'.$name.'</p>
-    //                 <h4>Email</h4><p>'.$email.'</p>
-    //                 <h4>Phone</h4><p>'.$phone.'</p>
-    //                 <h4>Date</h4><p>'.$date.'</p>
-    //                 <h4>Message</h4><p>'.$message.'</p>';
-    //             // Email Headers
-    //             $headers = "MIME-Version: 1.0" ."\r\n";
-    //             $headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
-    //             // Additional Headers
-    //             $headers .= "From: " .$name. "<".$email.">". "\r\n";
-    //             if(mail($email_to, $email_subject, $body, $headers)){
-    //                 // Email Sent
-    //                 echo "Thank you, your email has been sent and we'll get back to you soon.<br /><br />";
-    //                 echo '<a href="http://banibakery.com">Return home</a>';
-    //             } else {
-    //                 // Failed
-    //                 echo 'Your email was not sent';
-    //             }
-    //         }
-    //     }
-    // }
-
-
-    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //     $name = $_POST['name'];
-    //     $email = $_POST['email'];
-    //     $phone = $_POST['phone'];
-    //     $date = $_POST['date'];
-    //     $message = $_POST['message'];
-    //     $emailSuccess = "";
-    //     $emailFailed = "";
-    //     $nameErr = $emailErr = $phoneErr = $dateErr = $messageErr = "";
-
-    //     if(empty($name) && empty($email) && empty($phone) && empty($date) && empty($message)) {
-    //         // Failed
-    //         $nameErr = "Name is required";
-    //         $emailErr = "Email is required";
-    //         $phoneErr = "Phone number is required";
-    //         $dateErr = "Date is required";
-    //         $messageErr = "Message is required";
-    //     } else{
-    //         if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-    //             $emailErr = "Invalid email format";
-    //         } else {
-    //             $email_to = "banibakery@gmail.com";
-    //             $email_subject = 'Contact form submission from '.$name;
-    //             $body =
-    //                 '<h2>Contact Request</h2>
-    //                 <h4>Name</h4><p>'.$name.'</p>
-    //                 <h4>Email</h4><p>'.$email.'</p>
-    //                 <h4>Phone</h4><p>'.$phone.'</p>
-    //                 <h4>Date</h4><p>'.$date.'</p>
-    //                 <h4>Message</h4><p>'.$message.'</p>';
-    //             // Email Headers
-    //             $headers = "MIME-Version: 1.0" ."\r\n";
-    //             $headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
-    //             // Additional Headers
-    //             $headers .= "From: " .$name. "<".$email.">". "\r\n";
-    //             if(mail($email_to, $email_subject, $body, $headers)){
-    //                 // Email Sent
-    //                 // $emailSuccess = "Thank you, we will get back to you soon";
-    //             } else {
-    //                 // Failed
-    //                 // $emailFailed = "I'm sorry, something went wrong. Please try again";
-    //             }
-    //         }
-    //     }
-    // }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bani Bakery | Custom Cakes</title>
+
+    <link href="https://fonts.googleapis.com/css?family=DM+Serif+Text|Muli:300,700,800&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="images/bani_favicon.png">
+    <link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/queries.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+    <script src="js/lightbox-plus-jquery.min.js"></script>
+
+</head>
+<body>
+
+    <div>
+        <p class="alert alert-container <?php echo $msgClass ?>"><?php echo $msg ?></p>
+    </div>
+    
+</body>
+</html>
